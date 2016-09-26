10eb21363a663523c3934207ba2bcf2886304176
1719781450 <1719781450@qq.com>
2016-09-26 23:33:04 +0800

    Signed-off-by: 1719781450 <1719781450@qq.com>

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -2,7 +2,16 @@
 var util = require('../../utils/util.js')
 Page({
   data: {
-    logs: []
+    logs: [],
+    iconSize:[20,30,40,50,60,70],
+    iconColor:[
+      'red','orange','yellow','green','rgb(0,255,255)','blue','purple'
+    ],
+    iconType:[
+      'success','info','warn','waiting','safe_sucess','safe-warn',
+      'success_circle','success_no_circle','waiting_circle','circle','download',
+      'info_circle','cancle','search','clear'
+    ]
   },
   onLoad: function () {
     this.setData({
